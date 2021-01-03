ffc93adf897f768a13dfe125e7403a3fcbd031dd
yykunkun <yykunkun@126.com>
2021-01-04 00:19:39 +0800

    21费老了劲的公式算出来了，不知道哪错了，再算算应该问题不大

--- part3.py
+++ part3.py
@@ -6,18 +6,54 @@ class Solution:
     # 这个题就转化成了，圆心A移动前后，到圆心B的最小距离，是不是大于两个圆半径的和
 
     def IfIntersect(self, position):
-        # centerA1 = [position[0], position[1]]
         XA, YA = position[0], position[1]
+        print('圆A的圆心 ' + str(XA) + ' ' + str(YA))
         rA = position[2]
-        # centerB = [position[3], position[4]]
+        print('圆A半径 ' + str(rA))
         XB, YB = position[3], position[4]
+        print('圆B的圆心 ' + str(XB) + ' ' + str(YB))
         rB = position[5]
-        # P = [position[6], position[7]]
+        print('圆B半径 ' + str(rB))
         XP, YP = position[6], position[7]
+        print('P点坐标 ' + str(XP) + ' ' + str(YP))
         Xab, Yab = XB - XA, YB - YA  # 向量AB坐标
         Xap, Yap = XP - XA, YP - YA  # 向量AP坐标
         Xpa, Ypa = XA - XP, YA - YP  # 向量PA坐标
         Xpb, Ypb = XB - XP, YB - YP  # 向量PB坐标
         cosBAP = Xab * Xap + Yab * Yap
         cosBPA = Xpa * Xpb + Ypa * Ypb
-
+        # 直线AB的方程
+        # (XB-XA)y=(YB-YA)x+XBYA-XAYB
+        # (YB-YA)x+(XA-XB)y+XBYA-XAYB=0
+        # P到AB距离记作d,d的平方记作dd
+        dd = (((YB - YA) * XP + (XA - XB) * YP + XB * YA - XA * YB) ** 2) / ((YB - YA) ** 2 + (XA - XB) ** 2)
+        # 线段AB的平方记作ABAB
+        ABAB = (XB - XA) ** 2 + (YB - YA) ** 2
+        # 线段BP的平方记作BPBP
+        BPBP = (XP - XB) ** 2 + (YP - XB) ** 2
+        R = rA if rA >= rB else rB
+        r = rB if rA >= rB else rA
+        print('R= ' + str(R))
+        print('r= ' + str(r))
+        print('(R+r)平方 ' + str((R + r) ** 2))
+        print('(R-r)平方 ' + str((R - r) ** 2))
+        if cosBPA * cosBPA < 0:
+            # 小于零，一钝一锐，AP在B的同侧，比两个端点
+            longer = ABAB if ABAB > BPBP else BPBP
+            shorter = BPBP if ABAB > BPBP else ABAB
+            print(longer)
+            print(shorter)
+            if (R + r) ** 2 >= shorter and longer >= (R - r) ** 2:
+                return 1
+            else:
+                return -1
+        if cosBPA * cosBPA >= 0:
+            # 两个锐角，比长的那个端点，和垂线段距离
+            longer = ABAB if ABAB >= BPBP else BPBP
+            shorter = dd
+            print(longer)
+            print(shorter)
+            if (R + r) ** 2 >= shorter and longer >= (R - r) ** 2:
+                return 1
+            else:
+                return -1
